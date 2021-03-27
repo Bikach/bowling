e7501a14a13bb50a87e126f9095d2542c0335d8e
Abdoul Diallo <abdoul.diallo@soat.fr>
2021-03-27 09:34:37 +0100

    ajout de nested classe + gestion du spare

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -4,14 +4,22 @@ public class Bowling {
 
     private static final String STRIKE = "x";
     private static final String MISSED = "-";
+    private static final String SPARE = "/";
+
+    public int computeScore(String numberOfPinHitInFirstTrie, String numberOfPinHitInSecondTrie) {
+        if(SPARE.equals(numberOfPinHitInSecondTrie)) {
+            return 10;
+        }
+        return computeScore(numberOfPinHitInFirstTrie) + computeScore(numberOfPinHitInSecondTrie);
+    }
 
     public int computeScore(String numberOfPinHit) {
         if(STRIKE.equals(numberOfPinHit)) {
             return 10;
         }
-        if(MISSED.equals(numberOfPinHit)) {
+        else if(MISSED.equals(numberOfPinHit)) {
             return 0;
         }
-        return Integer.parseInt(numberOfPinHit);
+       return Integer.parseInt(numberOfPinHit);
     }
 }
--- src/test/java/BowlingTest.java
+++ src/test/java/BowlingTest.java
@@ -1,4 +1,5 @@
 import bowling.Bowling;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,24 +8,51 @@ class BowlingTest {
 
     private final Bowling bowling = new Bowling();
 
-    @Test
-    void should_return_1_when_one_pin_hit() {
-        assertThat(bowling.computeScore("1")).isEqualTo(1);
+    @Nested
+    class FirstTrie {
+        @Test
+        void should_return_1_when_one_pin_hit() {
+            assertThat(bowling.computeScore("1", "-")).isEqualTo(1);
+        }
+
+        @Test
+        void should_return_2_when_two_pins_hit() {
+            assertThat(bowling.computeScore("2", "-")).isEqualTo(2);
+        }
+
+        @Test
+        void should_return_10_when_have_strike() {
+            assertThat(bowling.computeScore("x", "-")).isEqualTo(10);
+        }
+
+        @Test
+        void should_return_0_when_missed() {
+            assertThat(bowling.computeScore("-", "-")).isZero();
+        }
+
+        @Test
+        void should_return_2_when_missed() {
+            assertThat(bowling.computeScore("-", "-")).isZero();
+        }
     }
 
-    @Test
-    void should_return_2_when_two_pins_hit() {
-        assertThat(bowling.computeScore("2")).isEqualTo(2);
-    }
+    @Nested
+    class TwoTrie {
 
-    @Test
-    void should_return_10_when_have_strike() {
-        assertThat(bowling.computeScore("x")).isEqualTo(10);
-    }
+        @Test
+        void should_return_2_when_two_pin_hit_after_two_trie() {
+            assertThat(bowling.computeScore("1", "1")).isEqualTo(2);
+            assertThat(bowling.computeScore("2", "-")).isEqualTo(2);
+            assertThat(bowling.computeScore("-", "2")).isEqualTo(2);
+        }
 
-    @Test
-    void should_return_0_when_missed() {
-        assertThat(bowling.computeScore("-")).isZero();
+        @Test
+        void should_return_10_when_spare_in_second_trie() {
+            assertThat(bowling.computeScore("1", "/")).isEqualTo(10);
+            assertThat(bowling.computeScore("2", "/")).isEqualTo(10);
+            assertThat(bowling.computeScore("3", "/")).isEqualTo(10);
+        }
+
     }
 
 }
