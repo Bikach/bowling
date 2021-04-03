d2366b9bae4449f7c2ce56a588be11dc4a8883fb
Abdoul Diallo <abdoul.diallo@soat.fr>
2021-04-03 16:40:03 +0200

    move buildFrames method

--- src/main/java/bowling/Bowling.java
+++ src/main/java/bowling/Bowling.java
@@ -1,14 +1,7 @@
 package bowling;
 
-import bowling.frame.*;
-import bowling.score.Score;
+import bowling.frame.FrameBuilder;
 import bowling.score.ScoreScanner;
-import bowling.score.SpareScore;
-import bowling.score.StrikeScore;
-
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Bowling {
 
@@ -22,28 +15,7 @@ public class Bowling {
     private Game buildGame(String party) {
         scoreScanner.scanParty(party);
 
-        return new Game(buildFrames());
+        return new Game(FrameBuilder.buildFrames(scoreScanner));
     }
 
-    private List<Frame> buildFrames() {
-        LinkedList<Frame> frames = new LinkedList<>(Collections.singletonList(new EmptyFrame()));
-        LinkedList<Score> scores = scoreScanner.getScores();
-
-        while(scoreScanner.hasScoresNotEmpty()) {
-            Frame lastFrame = frames.getLast();
-            Score firstScore = scores.removeFirst();
-            if(firstScore instanceof StrikeScore) {
-                frames.add(new StrikeFrame(scoreScanner.retrieveTwoNextScore(), lastFrame));
-                continue;
-            }
-
-            Score secondScore = scores.removeFirst();
-            if(secondScore instanceof SpareScore) {
-                frames.add(new SpareFrame(scoreScanner.retrieveNextScore(), lastFrame));
-            } else {
-                frames.add(new ClassicFrame(firstScore, secondScore, lastFrame));
-            }
-        }
-        return frames;
-    }
 }
--- src/main/java/bowling/frame/FrameBuilder.java
+++ src/main/java/bowling/frame/FrameBuilder.java
@@ -1,6 +1,14 @@
 package bowling.frame;
 
+import bowling.score.Score;
+import bowling.score.ScoreScanner;
+import bowling.score.SpareScore;
+import bowling.score.StrikeScore;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 public class FrameBuilder {
     private static final char CHAR_DELIMITER = '|';
@@ -12,5 +20,27 @@ public class FrameBuilder {
     public static String[] splitPartyToFrames(String party) {
         int nbFrame = StringUtils.countMatches(party, CHAR_DELIMITER);
         return party.split(FRAME_BOUNDARY, nbFrame);
+    }
+
+    public static List<Frame> buildFrames(ScoreScanner scoreScanner) {
+        LinkedList<Frame> frames = new LinkedList<>(Collections.singletonList(new EmptyFrame()));
+        LinkedList<Score> scores = scoreScanner.getScores();
+
+        while(scoreScanner.hasScoresNotEmpty()) {
+            Frame lastFrame = frames.getLast();
+            Score firstScore = scores.removeFirst();
+            if(firstScore instanceof StrikeScore) {
+                frames.add(new StrikeFrame(scoreScanner.retrieveTwoNextScore(), lastFrame));
+                continue;
+            }
+
+            Score secondScore = scores.removeFirst();
+            if(secondScore instanceof SpareScore) {
+                frames.add(new SpareFrame(scoreScanner.retrieveNextScore(), lastFrame));
+            } else {
+                frames.add(new ClassicFrame(firstScore, secondScore, lastFrame));
+            }
+        }
+        return frames;
     }
 }
